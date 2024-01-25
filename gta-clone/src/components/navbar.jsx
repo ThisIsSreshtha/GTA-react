087b7023afcd7743597aff6774a9f43ce33f60c8
Sambit <110420002+sambitsingha@users.noreply.github.com>
2024-01-25 23:47:23 +0530

    Add fontawesome dependencies and update navbar styling

--- gta-clone/src/components/navbar.jsx
+++ gta-clone/src/components/navbar.jsx
@@ -6,17 +6,33 @@ class Navbar extends Component {
 	render() {
 		return (
 			<>
-				<nav className='bg-black w-screen h-20 p-5 flex justify-center items-center m-auto'>
+				<nav className='bg-black text-white w-screen h-20 py-5 px-16 flex justify-center items-center m-auto'>
 					<img src={logo} alt='' className='w-12 h-8' />
 					<div className='bg-gray-400 w-[0.5px] h-9'></div>
 					<img src={gtaLogo} alt='' className='mx-3' />
-					<ul className='flex text-white '>
+					<ul className='flex justify-around items-center w-screen'>
 						<a href='#'>
 							<li>Overview</li>
+						</a>
+						<a href='#'>
 							<li>GTA Online</li>
+						</a>
+						<a href='#'>
 							<li>Story</li>
 						</a>
+						<a href='#'>
+							<li>GTA+</li>
+						</a>
+						<a href='#'>
+							<li>Community</li>
+						</a>
+						<a href='#'>
+							<li>Support</li>
+						</a>
 					</ul>
+					<button className=' w-40 h-10 border border-white rounded'>
+						Buy Now
+					</button>
 				</nav>
 			</>
 		);

6606a51b1a89142186e6c7ed61123942c91f7675
Sambit <110420002+sambitsingha@users.noreply.github.com>
2024-01-26 20:16:03 +0530

    Add hero pattern background image to tailwind.config.js

--- gta-clone/tailwind.config.js
+++ gta-clone/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
 	content: ["./src/**/*.{js,jsx,ts,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			backgroundImage: {
+				"hero-pattern": "url('./images/los-santos.jpg')",
+			},
+		},
 	},
 	plugins: [],
 };

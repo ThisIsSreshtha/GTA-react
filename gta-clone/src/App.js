21768bdb9adc8fd1ff785b3b41d8d8bdd9794b80
Sambit <110420002+sambitsingha@users.noreply.github.com>
2024-01-27 16:35:18 +0530

    Add Santos component to App.js

--- gta-clone/src/App.js
+++ gta-clone/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Navbar from "./components/navbar";
 import Hero from "./components/hero";
+import Santos from "./components/santos";
 function App() {
 	return (
 		<>
 			<Navbar />
 			<Hero />
+			<Santos />
 		</>
 	);
 }
